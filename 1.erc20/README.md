c9d5e9ad321a75b9d862e761f472be233f7167c6
xudan <875126243@qq.com>
2022-03-27 01:14:13 +0800

    ERC20 TEST

--- 1.erc20/README.md
+++ 1.erc20/README.md
@@ -1,2 +1,8 @@
 # learn-openzeppelin
 learn-openzeppelin
+
+
+
+###### 合约地址 0x8D0CD152eDCE6D3468884AD1ade809ce6A02e53f
+
+https://rinkeby.etherscan.io/address/0x8d0cd152edce6d3468884ad1ade809ce6a02e53f
---
+++ 1.erc20/demo4.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyDappToken is ERC20, Ownable {
+    constructor() ERC20("MyDappToken", "MDTK") {
+        _mint(msg.sender, 1000);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
+
+
+// 0x8D0CD152eDCE6D3468884AD1ade809ce6A02e53f  
